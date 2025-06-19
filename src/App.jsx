30f5c9768ff092786ef37ef0159a8b3efe7f81b2
Eyaas-Ajmal <eyaasofficial@gmail.com>
2025-06-19 11:42:22 +0530

    Hellowordl

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,10 @@ import React from 'react'
 
 const App = () => {
   return (
+    <div>
     <div className='bg-red-900'>App</div>
+    <h1 className='text-3xl font-bold underline text-black'>Hello world!</h1>
+    </div>
   )
 }
 
