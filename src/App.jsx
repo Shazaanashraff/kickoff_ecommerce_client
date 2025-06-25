5daf649b6aea2447e0db081188855909ccef5c98
ShazaanAshraff <mohamedshazaan7@gmail.com>
2025-06-25 18:01:43 +0530

    change

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import Hero from './components/Hero'
 import Navbar from './components/Navbar'
+import BestSellers from './components/BestSellers'
+import CategoriesBento from './components/CategoriesBento'
 
 const App = () => {
   return (
     <div className='min-h-screen bg-black'>
       <Navbar />
       <Hero />
+      <BestSellers />
+      <CategoriesBento />
     </div>
   )
 }
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Hero = () => {
   return (
     <div>
-        <img src='src/assets/3264.webp' className='p-10 w-full rounded-2xl opacity-80'/>
+        <img src='src\assets\Gemini_Generated_Image_h0oo95h0oo95h0oo.png' className='p-10 w-full min-h-screen rounded-2xl opacity-80'/>
     </div>
   )
 }
